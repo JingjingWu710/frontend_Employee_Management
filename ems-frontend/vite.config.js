81723ceda7f0d73ed4382ad376b16421bab0415d
JingjingWu710 <111849492+JingjingWu710@users.noreply.github.com>
2025-03-25 16:35:21 +0000

    Update vite.config.js

--- ems-frontend/vite.config.js
+++ ems-frontend/vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// export default defineConfig({
+//   plugins: [react()],
 
+//   server: {
+//     port: 3000
+//   }
+// })
+export default defineConfig({
   server: {
-    port: 3000
+    host: "0.0.0.0",
+    port: 8080
   }
-})
+});
